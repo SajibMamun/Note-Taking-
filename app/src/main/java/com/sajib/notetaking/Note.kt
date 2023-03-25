b6b7d252b37127afb361d247230aae41f195269e
Sajib Bin Mamun <sajibmamun138@gmail.com>
2023-03-26 05:57:13 +0600

    Data & Entity Class created

---
+++ app/src/main/java/com/sajib/notetaking/Note.kt
@@ -0,0 +1,17 @@
+package com.sajib.notetaking
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    var NoteID: Int,
+    @ColumnInfo(name = "note_title")
+    var NoteTitle: String,
+    @ColumnInfo(name = "note_description")
+    var NoteDetails: String
+
+
+)
