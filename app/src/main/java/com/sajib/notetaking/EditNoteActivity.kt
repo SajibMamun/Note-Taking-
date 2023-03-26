de307bf67fae514ac012cde5f9339d4bcb485432
Sajib Bin Mamun <sajibmamun138@gmail.com>
2023-03-26 14:39:30 +0600

    EditNoteActivity created

---
+++ app/src/main/java/com/sajib/notetaking/EditNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.sajib.notetaking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
