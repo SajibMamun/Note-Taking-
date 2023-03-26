fc103fb3591238b410affea75635751dff6fe526
Sajib Bin Mamun <sajibmamun138@gmail.com>
2023-03-26 07:07:48 +0600

    Intent to CreateNoteActivity

--- app/src/main/java/com/sajib/notetaking/MainActivity.kt
+++ app/src/main/java/com/sajib/notetaking/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.sajib.notetaking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.sajib.notetaking.activities.CreateNoteActivity
+import com.sajib.notetaking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.createnotebtn.setOnClickListener(View.OnClickListener {
+
+            var intent= Intent(this,CreateNoteActivity::class.java)
+            startActivity(intent)
+
+
+
+        })
     }
 }
